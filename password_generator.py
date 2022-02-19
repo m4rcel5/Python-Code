2f1a42e9a61a7569990726e47caf62ffb0a58f1a
m4rcel5 <marcelizak1@gmail.com>
2022-02-19 18:05:20 +0100

    This program generates random and safe passwords

---
+++ password_generator.py
@@ -0,0 +1,23 @@
+"""
+    @author: Marcel Lizak
+    19.02.2022
+    password generator
+"""
+import os
+import string
+import time
+import random
+
+os.system("cls")
+
+chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+length = input("Enter the length of your password (empty input stops the program): ")
+while length != "":
+    if int(length) <= 90 and int(length) >= 8:
+        password = "".join(random.sample(chars, int(length)))
+        print(f"Your password is: {password}")
+        time.sleep(2)
+        length = input("Enter the length of your password (empty input stops the program): ")
+    else:
+        print("The length must not be greater than 90 and must not be smaller than 8")
